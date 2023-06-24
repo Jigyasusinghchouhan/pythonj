9c77b571cbb64887cee8f6e24ea35222e9d94963
Jigyasusinghchouhan <jigyashu2001@gmail.com>
2023-06-24 22:15:47 +0530

    OK

--- bankAccount.py
+++ bankAccount.py
@@ -1,5 +1,5 @@
-#bankAccount.py
-class BankAccount():
+# bankAccount.py
+class BankAccount:
     # Default Values for Account Details
     counter = 0
     basic_salary = 0
@@ -11,7 +11,7 @@ class BankAccount():
         self.balance = balance
         BankAccount.counter += 1
 
-    def deposit(self, amount): 
+    def deposit(self, amount):
         self.balance += amount
         print("Deposit Accepted")
 
@@ -23,24 +23,24 @@ class BankAccount():
             print("Withdrawal Accepted")
 
     def find_interest(self):
-        interest = (self.balance * 0.05) 
+        interest = (self.balance * 0.05)
         self.balance += interest
 
     def __str__(self):
         return f"Owner: {self.owner}\nBalance: ${self.balance}"
 
     @classmethod
-    def fromString(cls, data_str):
-        lst = data_str.split()
+    def fromString(cls, data_st):
+        lst = data_st.split()
         return cls(lst[0], float(lst[1]))
 
     @classmethod
-    def fromList(cls, data_list):
-        return cls(data_list[0], data_list[1])
+    def fromList(cls, data_lis):
+        return cls(data_lis[0], data_lis[1])
 
     @classmethod
-    def fromDict(cls, data_dict):
-        return cls(list(data_dict.keys())[0], list(data_dict.values())[0])
+    def fromDict(cls, data_di):
+        return cls(list(data_di.keys())[0], list(data_di.values())[0])
 
     @staticmethod
     def display_details(acct):
@@ -50,29 +50,30 @@ class BankAccount():
         print(f"pf: {BankAccount.pf}")
         print(f"No. of accounts created :{BankAccount.counter}")
 
-#create account
-acct1 = BankAccount('Jose',100)
 
-#deposit
+# create account
+acct1 = BankAccount('Jose', 100)
+
+# deposit
 acct1.deposit(150)
 
-#withdraw
+# withdraw
 acct1.withdraw(75)
 
-#display details
+# display details
 BankAccount.display_details(acct1)
 
-#Add counter for an accounts
+# Add counter for an accounts
 BankAccount.counter += 1
 
-#Print all account details
+# Print all account details
 print(acct1)
 
 # Find the interest amount and add it to the account balance
 acct1.find_interest()
 print(acct1)
 
-#Take account details from the class object, string, list, and dictionary (Use class method)
+# Take account details from the class object, string, list, and dictionary (Use class method)
 data_str = "Steve 50"
 acct2 = BankAccount.fromString(data_str)
 print(acct2)
@@ -81,6 +82,6 @@ data_list = ["Dave", 100]
 acct3 = BankAccount.fromList(data_list)
 print(acct3)
 
-data_dict = {"Alex":200}
+data_dict = {"Alex": 200}
 acct4 = BankAccount.fromDict(data_dict)
 print(acct4)
